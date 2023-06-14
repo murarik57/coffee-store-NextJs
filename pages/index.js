c39b503ec2d8d074d69688c9f3cb2f461bf9d2c3
murarik57 <murarikumar57@gmail.com>
2023-06-14 11:01:52 +0530

    added custom download file

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Banner from "../components/Banner";
 import { useCallback } from "react";
+import Image from "next/image";
 
 const Home = () => {
   const handleClick = useCallback(() => {
@@ -18,6 +19,14 @@ const Home = () => {
 
       <main className={styles.main}>
         <Banner buttonText={"View stores nearby"} handleClick={handleClick} />
+        <div className={styles.heroImage}>
+          <Image
+            src="/static/images/banner-image.png"
+            width={700}
+            height={400}
+            alt=""
+          />
+        </div>
       </main>
     </div>
   );
